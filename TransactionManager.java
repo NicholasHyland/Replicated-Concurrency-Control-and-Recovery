41fc7e22e6eabedbde54e7914745aba49e98f827
Palak Bhasin <palak.bhasin@hotmail.com>
2017-10-31 10:42:07 -0400

    Added to TM

--- TransactionManager.java
+++ TransactionManager.java
@@ -6,12 +6,13 @@ public class TransactionManager {
 	ArrayList<Operation> operations;
 	ArrayList<Transaction> transactions;
 	ArrayList<Variable> variables;
-	int runningTransactions;
+	int runningTransactions, currentTime;
 
 
 	public TransactionManager(ArrayList<Operation> operations){
 		this.operations = operations;
 		runningTransactions=0;
+		currentTime=0;
 	}
 
 	//initializes all 10 sites
@@ -23,10 +24,32 @@ public class TransactionManager {
   	}
 
 	public void simulate(){
+		Operation current;
+		//loop until the current operation is not a begin
+		for (Operation o: operations){
+			current=o;
+			if (o.operationType.equals("begin")){
+				transactions.add(New Transaction(o.transactionName, false))
+			}
+			else if (o.operationType.equals("beginRO"))
+				transactions.add(New Transaction(o.transactionName, true))
+			runningTransactions++;
+			else break;
+		}
 
+		//loop until all transactions have ended, detect deadlocks at every iteration, iterate time with every loop
+		while (runningTransactions>0){
+
+
+
+			detectDeadlock();
+			currentTime++;
+		}
 	}
 
 	public void detectDeadlock(){
+		if (runningTransactions==1)
+			return;
 
 	}
 }
