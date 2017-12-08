163f56b060e20539b84eb994f4c94d00c80c1d1f
Palak Bhasin <palak.bhasin@hotmail.com>
2017-12-07 21:40:43 -0500

    Run tests in order

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!bin/bash
 javac RepCRec.java
-for i in $(ls Tests/Test*.txt); do java RepCRec $(basename $i); done
+#for i in $(ls Tests/Test*.txt); do java RepCRec $(basename $i); done
 #java RepCRec Test1.txt 
+for ((i=1;i<4;i++))
+{
+java RepCRec Test$i.txt
+}
+
+java RepCRec Test3.5.txt
+
+for ((i=4;i<22;i++))
+{
+java RepCRec Test$i.txt
+}
